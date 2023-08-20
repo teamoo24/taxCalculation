0c8e8e21f5253d5a880272646fc0db757f2b78be
Han kyungsoo <dripcoke24@gmail.com>
2023-08-20 20:39:27 +0900

    基本的な機能は実装

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <p>原価：<input type="number" v-model="price"></p>
+    <p>税率：<input type="number" v-model="tax"></p>
+    <h1>支払額：{{ price*tax }}</h1>
   </div>
 </template>
 <script>
   export default {
     data: () => ({
-      message: "tax test"
+      price: "",
+      tax:""
     })
   }
 </script>
