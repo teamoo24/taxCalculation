ac787808ce5602f7376d34bb6c1448825a273c3b
Han kyungsoo <dripcoke24@gmail.com>
2023-08-20 20:46:08 +0900

    単位数修正

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <p>原価(¥)：<input type="number" v-model="price"></p>
     <p>税率(%)：<input type="number" v-model="tax"></p>
-    <h1>支払額：{{ price+(price*(tax*0.01)) }}</h1>
+    <h1>支払額：{{ price+(price*(tax*0.01)) }}(¥)</h1>
   </div>
 </template>
 <script>
