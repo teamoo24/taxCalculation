90b3a4f307320a686f511390c30169fd80cc5a5e
Han kyungsoo <dripcoke24@gmail.com>
2023-08-20 20:43:28 +0900

    基本機能実装

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <p>原価：<input type="number" v-model="price"></p>
-    <p>税率：<input type="number" v-model="tax"></p>
-    <h1>支払額：{{ price*tax }}</h1>
+    <p>原価(¥)：<input type="number" v-model="price"></p>
+    <p>税率(%)：<input type="number" v-model="tax"></p>
+    <h1>支払額：{{ price+(price*(tax*0.01)) }}</h1>
   </div>
 </template>
 <script>
   export default {
     data: () => ({
-      price: "",
-      tax:""
+      price: 1000,
+      tax:10
     })
   }
 </script>
